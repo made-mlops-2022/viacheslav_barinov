75c9c42dce5fd479ca7e1beb3dbab0a62deafa53
Viacheslav <slava.barinov2002@yandex.ru>
2022-12-28 17:27:36 +0300

    Some refactor changes

--- dags/train.py
+++ dags/train.py
@@ -17,19 +17,24 @@ MOUNT_SOURCE = Mount(
     target="/data",
     type='bind'
     )
-
-default_args = {
-    "owner": "airflow",
-    "email": ["airflow@example.com"],
-    "retries": 1,
-    "retry_delay": timedelta(minutes=5),
+    
+DEFAULT_PARAMS = {
+    "do_xcom_push":False,
+    "network_mode":"bridge",
+    "mounts":[MOUNT_SOURCE]
 }
+
+
 
 
 with DAG(
         "train",
-        default_args=default_args,
-        schedule_interval="@daily",
+        default_args={
+    		"owner": "airflow",
+    		"email": ["airflow@example.com"],
+    		"retries": 1,
+    		"retry_delay": timedelta(minutes=5)},
+        schedule_interval="@weekly",
         start_date=days_ago(0),
 ) as dag:
 
@@ -47,36 +52,28 @@ with DAG(
         image="airflow-data-preprocess",
         command=f"--dir_from {GENERATE_DIR_NAME} --dir_in {PROCESSED_DIR_NAME} --transform_dir {TRANSFORMER_DIR_NAME}",
         task_id="docker-airflow-preprocess",
-        do_xcom_push=False,
-        network_mode="bridge",
-        mounts=[MOUNT_SOURCE]
+        **DEFAULT_PARAMS
     )
 
     split_data = DockerOperator(
         image="airflow-data-split",
         command=f"--dir_from {PROCESSED_DIR_NAME} --val_size {VAL_SIZE}",
         task_id="docker-airflow-split",
-        do_xcom_push=False,
-        network_mode="bridge",
-        mounts=[MOUNT_SOURCE]
+        **DEFAULT_PARAMS
     )
 
     train_model = DockerOperator(
         image="airflow-train",
         command=f"--dir_from {PROCESSED_DIR_NAME} --dir_in {MODEL_DIR_NAME}",
         task_id="docker-airflow-train",
-        do_xcom_push=False,
-        network_mode="bridge",
-        mounts=[MOUNT_SOURCE]
+        **DEFAULT_PARAMS
     )
 
     val_model = DockerOperator(
         image="airflow-validation",
         command=f"--model_dir_from {MODEL_DIR_NAME} --data_dir_from {PROCESSED_DIR_NAME} --metric_dir {METRICS_DIR_NAME}",
         task_id="docker-airflow-valid",
-        do_xcom_push=False,
-        network_mode="bridge",
-        mounts=[MOUNT_SOURCE]
+        **DEFAULT_PARAMS
     )
 
     [features_sensor, targets_sensor] >> preprocess_data
